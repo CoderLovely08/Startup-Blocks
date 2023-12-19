5f3e89fd2c7fe5c5b01940f258b4681d8d6e1ac9
Lovely Sharma <lovelysingeras297@gmail.com>
2023-12-20 01:01:27 +0530

    Login api compelted

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,3 +1,6 @@
+import { generateJwtToken } from "../middlewares/tokenHandler.js";
+import { authenticateUser } from "../modules/auth.js";
+
 export const registerUser = async (req, res) => {
   try {
     res.json({
@@ -6,6 +9,53 @@ export const registerUser = async (req, res) => {
   } catch (error) {
     res.json({
       error: "Error occured in registration route",
+    });
+  }
+};
+
+export const loginUser = async (req, res) => {
+  try {
+    const { userEmail, userPassword } = req.body;
+
+    // Use a service function to authenticate the user
+    const authenticationResult = await authenticateUser(
+      userEmail,
+      userPassword
+    );
+
+    if (authenticationResult.authenticated) {
+      // If the user is authenticated, generate a JWT token
+      const token = await generateJwtToken({
+        userId: authenticationResult.userId,
+        userName: authenticationResult.userFullName,
+      });
+
+      // Set the JWT as an HttpOnly cookie for better security
+      res.cookie("token", token, {
+        // httpOnly: true,
+        // secure: process.env.NODE_ENV === "production",
+      });
+
+      // Respond with success and the token
+      res.json({
+        success: true,
+        message: "Login Successful",
+        userName: authenticationResult.userFullName,
+      });
+    } else {
+      // If authentication fails, respond with an error message
+      res.status(200).json({
+        success: false,
+        message: "Invalid credentials",
+      });
+    }
+  } catch (error) {
+    console.error("Error in login route:", error);
+
+    // Handle other errors and respond with an appropriate message
+    res.status(500).json({
+      success: false,
+      message: "Internal server error",
     });
   }
 };
