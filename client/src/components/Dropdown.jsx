efb266722d0adc115c8932d2a7f78d775a534fc0
Lovely Sharma <lovelysingeras297@gmail.com>
2023-12-20 01:47:52 +0530

    Added filter functionality

---
+++ client/src/components/Dropdown.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { useStartups } from "../context/StartupContext";
+
+const Dropdown = ({ options, title }) => {
+  const { dispatch } = useStartups();
+
+  return (
+    <select
+      className="bg-slate-100 px-2 py-1 mx-2 shadow-md border rounded-md"
+      onChange={(e) => {
+        dispatch({
+          type: "SET_CURRENT_FILTER",
+          payload: e.target.value != "All" ? e.target.value : "",
+        });
+        dispatch({ type: "SET_CURRENT_PAGE", payload: 1 });
+      }}
+      defaultValue={title}
+    >
+      <option disabled hidden>
+        {title}
+      </option>
+      {options.map((option) => (
+        <option key={option.startup_investment_type}>
+          {option.startup_investment_type}
+        </option>
+      ))}
+    </select>
+  );
+};
+
+export default Dropdown;
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,40 +1,60 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
+import Button from "../components/Button";
 import Card from "../components/Card";
+import Dropdown from "../components/Dropdown";
 import Hero from "../components/Hero";
+import LoadingSpinner from "../components/LoadingSpinner";
 import Modal from "../components/Modal";
+import { useStartups } from "../context/StartupContext";
+import { BASE_STARTUP_URL } from "../service";
 
 const Home = () => {
   const [selectedStartup, setSelectedStartup] = useState(null);
-  const [startupList, setStartupList] = useState([]);
+  const [filterOptions, setFilterOptions] = useState([]);
+
+  const { state, dispatch } = useStartups();
+  const { startupItems, loading, searchQuery, currentPage, filterValue } =
+    state;
 
   useEffect(() => {
+    const fetchTypes = async () => {
+      const filterOptionsData = await axios.get(
+        `${BASE_STARTUP_URL}/investments`
+      );
+
+      setFilterOptions((prev) =>
+        filterOptionsData.data.data.filter(
+          (item) => item.startup_investment_type.length > 0
+        )
+      );
+    };
+    fetchTypes();
+  }, []);
+
+  useEffect(() => {
+    console.log("claled");
     const fetchData = async () => {
       try {
-        const response = await axios.get(
-          "http://localhost:3000/api/startup/startups"
+        dispatch({ type: "SET_LOADING", payload: true });
+
+        const startupData = await axios.get(
+          `${BASE_STARTUP_URL}/startups?search=${searchQuery}&page=${currentPage}&filter=${filterValue}`
         );
-        console.log(response.data.data);
-        setStartupList(response.data.data);
+
+        dispatch({
+          type: "SET_STARTUP_ITEMS",
+          payload: startupData.data.data,
+        });
       } catch (error) {
         console.error("Error fetching data:", error);
+      } finally {
+        dispatch({ type: "SET_LOADING", payload: false });
       }
     };
 
     fetchData(); // Call the async function
-  }, []); // Empty dependency array means this effect runs once when the component mounts
-
-  const startups = [
-    {
-      startupName: "Startup 1",
-      industryVertical: "Tech",
-      subVertical: "AI",
-      city: "San Francisco",
-      funding: 1000000,
-      remarks: "Innovative AI solutions.",
-    },
-    // Add more startup objects as needed
-  ];
+  }, [currentPage, searchQuery, filterValue]);
 
   const handleStartupClick = (startup) => {
     setSelectedStartup(startup);
@@ -44,23 +64,59 @@ const Home = () => {
     setSelectedStartup(null);
   };
 
-  const items = [
-    { title: "Cred", funding: 1234567, date: new Date(), city: "Nagpur" },
-  ];
+  const handleLoadMore = () => {
+    if (!loading) {
+      dispatch({ type: "SET_CURRENT_PAGE", payload: currentPage + 1 });
+    }
+  };
+
+  const handleApplyFilter = () => {
+    // Trigger a re-fetch of data based on the applied filter
+    dispatch({ type: "SET_CURRENT_FILTER", payload: filterValue }); // Reset currentPage to 1
+  };
+
   return (
-    <main className="relative flex flex-col items-center max-md:px-4">
+    <main className="relative flex flex-col items-center max-md:px-4 bg-slate-100 min-h-screen">
       {/* Hero Section */}
       <Hero />
       {/* Cards Section */}
+      <div className="flex w-2/3 max-sm:w-full items-center justify-end mx-2 my-2">
+        <span className="font-bold text-xl">Filters: </span>
+        <Dropdown
+          title={"Investment Type"}
+          options={[{ startup_investment_type: "All" }, ...filterOptions]}
+        />
+        {/* <Button label={"Apply"} onClick={handleApplyFilter} /> */}
+      </div>
+
       <section className="w-2/3 bg-white border rounded-md flex flex-wrap justify-center max-md:w-full">
-        {startupList.map((item) => (
-          <Card
-            key={item.title}
-            startupInfo={item}
-            handleClick={() => handleStartupClick(item)}
-          />
-        ))}
+        {startupItems.length > 0 ? (
+          startupItems.map((item) => (
+            <Card
+              key={item.startup_id}
+              startupInfo={item}
+              handleClick={() => handleStartupClick(item)}
+            />
+          ))
+        ) : loading ? (
+          <LoadingSpinner />
+        ) : (
+          <div className="text-xl">
+            Uh ohh! Nothing to see here, try some different search query
+          </div>
+        )}
       </section>
+
+      {/* Load More Button */}
+      {loading ? (
+        <LoadingSpinner />
+      ) : startupItems.length > 0 ? (
+        <div className="m-4">
+          <Button label={"Load More"} onClick={handleLoadMore} />
+        </div>
+      ) : (
+        ""
+      )}
       {/* Render the Modal */}
       {selectedStartup && (
         <Modal startupInfo={selectedStartup} onClose={handleCloseModal} />
