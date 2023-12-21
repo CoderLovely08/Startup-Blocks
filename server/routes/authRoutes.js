fc610fc42626b0ebc202f951526f2a391085356b
Lovely Sharma <lovelysingeras297@gmail.com>
2023-12-21 06:06:13 +0530

    Added comments

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -6,13 +6,19 @@ import {
   validateUser,
 } from "../controllers/authController.js";
 import { verifyToken } from "../middlewares/tokenHandler.js";
+
 const router = express.Router();
 
+// Route to register a new user
 router.route("/register").post(registerUser);
 
+// Route to login an existing user
 router.route("/login").post(loginUser);
 
+// Route to validate and get information about an authenticated user, requires token verification
 router.route("/validate").get(verifyToken, validateUser);
 
+// Route to log out the currently authenticated user
 router.route("/logout").post(logoutUser);
+
 export default router;
