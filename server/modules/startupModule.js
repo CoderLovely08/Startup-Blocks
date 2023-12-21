026563587a312f1ef3cbaf532dc5fd98f4997a97
Lovely Sharma <lovelysingeras297@gmail.com>
2023-12-21 06:04:24 +0530

    Added method for adding new startup data to db

--- server/modules/startupModule.js
+++ server/modules/startupModule.js
@@ -44,6 +44,10 @@ export const getAllStartups = async (searchString, page, pageSize, filter) => {
   }
 };
 
+/**
+ * Get all distinct investment types from the StartupInfo table.
+ * @returns {Array} An array containing distinct investment types.
+ */
 export const getAllInvestmentTypes = async () => {
   try {
     const query = {
@@ -56,3 +60,68 @@ export const getAllInvestmentTypes = async () => {
     return [];
   }
 };
+
+/**
+ * Add a new startup item to the StartupInfo table.
+ * @param {string} startupCity - The city where the startup is located.
+ * @param {string} startupDate - The date the startup was established.
+ * @param {number} startupFundingAmount - The funding amount for the startup.
+ * @param {string} startupIndustryVertical - The industry vertical of the startup.
+ * @param {string} startupInvestmentType - The type of investment for the startup.
+ * @param {string} startupInvestorName - The name of the investor in the startup.
+ * @param {string} startupName - The name of the startup.
+ * @param {string} startupRemarks - Remarks or additional information about the startup.
+ * @param {string} startupSubVertical - The sub-vertical of the startup.
+ * @param {string} userId - The ID of the user associated with the startup.
+ * @returns {boolean} True if the startup item was added successfully, false otherwise.
+ */
+export const addNewStartupItem = async (
+  startupCity,
+  startupDate,
+  startupFundingAmount,
+  startupIndustryVertical,
+  startupInvestmentType,
+  startupInvestorName,
+  startupName,
+  startupRemarks,
+  startupSubVertical,
+  userId
+) => {
+  try {
+    const query = {
+      text: `
+      INSERT INTO StartupInfo(
+        startup_name,
+        startup_industry_vertical,
+        startup_sub_vertical,
+        startup_city,
+        startup_investor_name,
+        startup_investment_type,
+        startup_funding,
+        startup_date,
+        startup_remarks,
+        user_id
+      ) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10)
+      `,
+      values: [
+        startupName,
+        startupIndustryVertical,
+        startupSubVertical,
+        startupCity,
+        startupInvestorName,
+        startupInvestmentType,
+        startupFundingAmount,
+        startupDate,
+        startupRemarks,
+        userId,
+      ],
+    };
+
+    const { rowCount } = await pool.query(query);
+    return rowCount === 1;
+  } catch (error) {
+    console.error(`Error in addNewStartupItem() call: ${error}`);
+    return false;
+  }
+};
+
